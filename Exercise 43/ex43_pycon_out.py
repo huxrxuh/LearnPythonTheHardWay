a1574ada3bab2dc270981d33abbfea03c84c9a84
Rodrigo Huelitl <huelitl.x@gmail.com>
2025-05-29 21:04:10 -0600

    chore: add ex43

--- Exercise 43/ex43_pycon_out.py
+++ Exercise 43/ex43_pycon_out.py
@@ -5,3 +5,29 @@ things[1] = 'z'
 print(things[1])
 
 print(things)
+
+stuff = {'name': 'Zed', 'age': 39, 'height': 6 * 12 + 2}
+print(stuff['name'])
+
+print(stuff['age'])
+
+print(stuff['height'])
+
+stuff['city'] = 'SF'
+print(stuff['city'])
+
+stuff[1] = 'wow'
+
+stuff[2] = 'Neato'
+
+print(stuff[1])
+
+print(stuff[2])
+
+stuff.pop('city')
+
+stuff.pop(1)
+
+stuff.pop(2)
+
+print(stuff)
